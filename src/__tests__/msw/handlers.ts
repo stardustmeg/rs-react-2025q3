e0d07f5c40804a524d8974005f371c98dcfd3d4c
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-07-05 18:26:08 +0300

    test: cover api service

--- src/__tests__/msw/handlers.ts
+++ src/__tests__/msw/handlers.ts
@@ -1,1 +1,29 @@
-export const handlers = [];
+import { http, HttpResponse } from 'msw';
+
+import { mockCharacters } from '@/__mocks__/mockCharacters';
+
+export const handlers = [
+  http.get(`*/character`, ({ request }) => {
+    const name = new URL(request.url).searchParams.get('name');
+
+    if (!name) {
+      return HttpResponse.json({
+        info: { count: 0, next: null, pages: 0, prev: null },
+        results: mockCharacters,
+      });
+    }
+
+    if (name === 'rick') {
+      return HttpResponse.json({
+        info: { count: 0, next: null, pages: 0, prev: null },
+        results: mockCharacters,
+      });
+    }
+
+    if (name === 'status 500') {
+      return new HttpResponse(null, { status: 500 });
+    }
+
+    return new HttpResponse(null, { status: 404 });
+  }),
+];
