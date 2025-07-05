eeaadae9fac0d6967f4e3718dcfa449bf0219a34
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-07-05 16:47:56 +0300

    test: cover Error button component

---
+++ src/components/__tests__/ErrorButton.spec.tsx
@@ -0,0 +1,32 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+
+import { MockErrorBoundary } from '@/__mocks__/MockErrorBoundary';
+import ErrorButton from '@/components/ErrorButton';
+
+describe('ErrorButton', () => {
+  it('renders button initially', () => {
+    render(
+      <MockErrorBoundary>
+        <ErrorButton />
+      </MockErrorBoundary>,
+    );
+    expect(screen.getByRole('button')).toBeInTheDocument();
+  });
+
+  it('catches error when button clicked', () => {
+    render(
+      <MockErrorBoundary>
+        <ErrorButton />
+      </MockErrorBoundary>,
+    );
+
+    fireEvent.click(screen.getByRole('button'));
+    expect(screen.getByRole('alert')).toHaveTextContent('Test error thrown from ErrorButton');
+  });
+
+  it('matches snapshot', () => {
+    const { asFragment } = render(<ErrorButton />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
