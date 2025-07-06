24823e2bf88bd58d079c74c85ccda6f5a396bd33
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-07-07 02:37:20 +0300

    refactor: upgrade Loader to a functional component

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,36 +1,32 @@
-import React, { PureComponent } from 'react';
+import React, { type JSX } from 'react';
 
 import styles from '@/components/Loader/Loader.module.css';
 
-class Loader extends PureComponent {
-  public override render(): React.ReactNode {
-    return (
-      <div className={styles.loaderWrapper}>
-        <div
-          aria-label="Orange and tan hamster running in a metal wheel"
-          className={styles.wheelAndHamster}
-          data-testid="loader-spinner"
-        >
-          <div className={styles.wheel} />
-          <div className={styles.hamster}>
-            <div className={styles.hamsterBody}>
-              <div className={styles.hamsterHead}>
-                <div className={styles.hamsterEar} />
-                <div className={styles.hamsterEye} />
-                <div className={styles.hamsterNose} />
-              </div>
-              <div className={styles.hamsterLimb + ' ' + styles.hamsterLimbFr} />
-              <div className={styles.hamsterLimb + ' ' + styles.hamsterLimbFl} />
-              <div className={styles.hamsterLimb + ' ' + styles.hamsterLimbBr} />
-              <div className={styles.hamsterLimb + ' ' + styles.hamsterLimbBl} />
-              <div className={styles.hamsterTail} />
-            </div>
+const Loader = (): JSX.Element => (
+  <div className={styles.loaderWrapper}>
+    <div
+      aria-label="Orange and tan hamster running in a metal wheel"
+      className={styles.wheelAndHamster}
+      data-testid="loader-spinner"
+    >
+      <div className={styles.wheel} />
+      <div className={styles.hamster}>
+        <div className={styles.hamsterBody}>
+          <div className={styles.hamsterHead}>
+            <div className={styles.hamsterEar} />
+            <div className={styles.hamsterEye} />
+            <div className={styles.hamsterNose} />
           </div>
-          <div className={styles.spoke} />
+          <div className={`${styles.hamsterLimb} ${styles.hamsterLimbFr}`} />
+          <div className={`${styles.hamsterLimb} ${styles.hamsterLimbFl}`} />
+          <div className={`${styles.hamsterLimb} ${styles.hamsterLimbBr}`} />
+          <div className={`${styles.hamsterLimb} ${styles.hamsterLimbBl}`} />
+          <div className={styles.hamsterTail} />
         </div>
       </div>
-    );
-  }
-}
+      <div className={styles.spoke} />
+    </div>
+  </div>
+);
 
-export default Loader;
+export default React.memo(Loader);
