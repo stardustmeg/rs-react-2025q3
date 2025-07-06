9e6817215cf7efcbf2cacff38768489433a4426b
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-07-07 02:48:58 +0300

    refactor: upgrade Character card to a functional component

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { type JSX } from 'react';
 
 import type { Character } from '@/types';
 
@@ -12,37 +12,34 @@ interface Props {
   character: Character;
 }
 
-class CharacterCard extends PureComponent<Props> {
-  public override render(): React.ReactNode {
-    const { character } = this.props;
-    const { gender, image, name, origin, species, status } = character;
+const CharacterCard = ({ character }: Props): JSX.Element => {
+  const { gender, image, name, origin, species, status } = character;
 
-    const info: [label: LabelsType, value: string][] = [
-      [LABELS.origin, origin.name],
-      [LABELS.species, species],
-      [LABELS.gender, gender],
-      [LABELS.status, status],
-    ];
+  const info: [label: LabelsType, value: string][] = [
+    [LABELS.origin, origin.name],
+    [LABELS.species, species],
+    [LABELS.gender, gender],
+    [LABELS.status, status],
+  ];
 
-    return (
-      <div
-        className="mx-auto w-68 rounded-lg bg-white p-2 shadow-md transition-transform duration-300 hover:scale-101"
-        data-testid="character-card"
-      >
-        <div className="flex flex-col items-center rounded-t-lg bg-custom-beige p-4">
-          <p className="mb-2 text-center text-lg font-semibold">{name}</p>
-          <CharacterImage alt={name} src={image} />
-        </div>
-        <div className="space-y-2 rounded-b-lg bg-white py-4 text-sm">
-          {info.map(([label, value]) => (
-            <p key={label}>
-              <span className="font-semibold">{label}:</span> {value}
-            </p>
-          ))}
-        </div>
+  return (
+    <div
+      className="mx-auto w-68 rounded-lg bg-white p-2 shadow-md transition-transform duration-300 hover:scale-101"
+      data-testid="character-card"
+    >
+      <div className="flex flex-col items-center rounded-t-lg bg-custom-beige p-4">
+        <p className="mb-2 text-center text-lg font-semibold">{name}</p>
+        <CharacterImage alt={name} src={image} />
       </div>
-    );
-  }
-}
+      <div className="space-y-2 rounded-b-lg bg-white py-4 text-sm">
+        {info.map(([label, value]) => (
+          <p key={label}>
+            <span className="font-semibold">{label}:</span> {value}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default CharacterCard;
+export default React.memo(CharacterCard);
