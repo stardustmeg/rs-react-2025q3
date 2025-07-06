b6fe710415ce24db7ace9c6c20b0d143643e2b95
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-07-07 02:58:14 +0300

    refactor: upgrade Character image to a functional component

--- src/components/CharacterImage.tsx
+++ src/components/CharacterImage.tsx
@@ -1,51 +1,40 @@
-import React, { PureComponent } from 'react';
+import React, { type JSX, useState } from 'react';
 
 import fallbackImage from '@/assets/png/placeholder.png';
 import Skeleton from '@/components/Skeleton';
+
+const DEFAULT_ALT_TEXT = 'Character image not available';
 
 interface Props {
   alt: string;
   src: string;
 }
 
-interface State {
-  loaded: boolean;
-}
+const CharacterImage = ({ alt, src }: Props): JSX.Element => {
+  const [loaded, setLoaded] = useState(false);
 
-class CharacterImage extends PureComponent<Props, State> {
-  private static readonly DEFAULT_ALT_TEXT = 'Character image not available';
-
-  public override state = { loaded: false };
-
-  public override render(): React.ReactNode {
-    const { alt, src } = this.props;
-    const { loaded } = this.state;
-
-    return (
-      <div className="relative h-full min-h-56 w-full min-w-56 overflow-hidden rounded">
-        {!loaded && <Skeleton />}
-        <img
-          alt={alt}
-          className={`h-full w-full object-cover transition-opacity duration-300 ${
-            loaded ? 'opacity-100' : 'opacity-0'
-          }`}
-          onError={this.handleError}
-          onLoad={this.markAsLoaded}
-          src={src}
-        />
-      </div>
-    );
-  }
-
-  private readonly handleError = (event: React.SyntheticEvent<HTMLImageElement>): void => {
+  const handleError = (event: React.SyntheticEvent<HTMLImageElement>): void => {
     event.currentTarget.src = fallbackImage;
-    event.currentTarget.alt = CharacterImage.DEFAULT_ALT_TEXT;
-    this.markAsLoaded();
+    event.currentTarget.alt = DEFAULT_ALT_TEXT;
+    markAsLoaded();
   };
 
-  private readonly markAsLoaded = (): void => {
-    this.setState({ loaded: true });
+  const markAsLoaded = (): void => {
+    setLoaded(true);
   };
-}
 
-export default CharacterImage;
+  return (
+    <div className="relative h-full min-h-56 w-full min-w-56 overflow-hidden rounded">
+      {!loaded && <Skeleton />}
+      <img
+        alt={alt}
+        className={`h-full w-full object-cover transition-opacity duration-300 ${loaded ? 'opacity-100' : 'opacity-0'}`}
+        onError={handleError}
+        onLoad={markAsLoaded}
+        src={src}
+      />
+    </div>
+  );
+};
+
+export default React.memo(CharacterImage);
