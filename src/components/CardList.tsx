9a3fb03c004bb1537c4c4153223770ffb71cecc6
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-07-06 18:39:12 +0300

    refactor: add components ids

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -49,7 +49,10 @@ class CardList extends PureComponent<Props, State> {
     }
 
     return (
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div
+        className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+        data-testid="card-list"
+      >
         {characters.map((char) => (
           <CharacterCard character={char} key={char.id} />
         ))}
