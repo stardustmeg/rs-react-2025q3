0f350f4c01bcd0a9a135c999e2029d11e0f8ae20
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-07-07 02:40:55 +0300

    refactor: upgrade No results found to a functional component

--- src/components/NoResultsFound.tsx
+++ src/components/NoResultsFound.tsx
@@ -1,20 +1,16 @@
-import React, { PureComponent } from 'react';
+import React, { type JSX } from 'react';
 
 import portalImage from '@/assets/gif/portal-rick-and-morty.gif';
 
-class NoResultsFound extends PureComponent {
-  public override render(): React.ReactNode {
-    return (
-      <div className="flex flex-col items-center justify-center space-y-6 p-6 text-center">
-        <p className="max-w-md text-lg text-custom-dark-night">
-          Nothing was found in this dimension.
-          <br />
-          Try a different one.
-        </p>
-        <img alt="No results found" className="h-auto w-64" src={portalImage} />
-      </div>
-    );
-  }
-}
+const NoResultsFound = (): JSX.Element => (
+  <div className="flex flex-col items-center justify-center space-y-6 p-6 text-center">
+    <p className="max-w-md text-lg text-custom-dark-night">
+      Nothing was found in this dimension.
+      <br />
+      Try a different one.
+    </p>
+    <img alt="No results found" className="h-auto w-64" src={portalImage} />
+  </div>
+);
 
-export default NoResultsFound;
+export default React.memo(NoResultsFound);
