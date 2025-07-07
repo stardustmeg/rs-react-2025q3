eece5ebd17e78fd9e7bca1a0a02680c8954e6565
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-07-07 05:54:19 +0300

    refactor: upgrade App to a functional component

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { type JSX, useEffect, useState } from 'react';
 
 import portal from '@/assets/gif/portal-rick-and-morty.gif';
 import errorImage from '@/assets/png/rick_and_morty.png';
@@ -6,37 +6,30 @@ import CardList from '@/components/CardList';
 import Header from '@/components/Header';
 import { getTrimmedSearchQuery } from '@/services/localStorage';
 
-interface State {
-  search: string;
-}
-
-class App extends Component<object, State> {
-  public override state = { search: getTrimmedSearchQuery() };
-
-  public override componentDidMount(): void {
-    this.preloadMedia();
+const preloadMedia = (): void => {
+  for (const source of [portal, errorImage]) {
+    const img = new Image();
+    img.src = source;
   }
+};
 
-  public override render(): React.ReactNode {
-    const { search } = this.state;
-    return (
-      <div className="w-full p-10">
-        <Header onSearch={this.handleSearch} />
-        <CardList search={search} />
-      </div>
-    );
-  }
+const App = (): JSX.Element => {
+  const [search, setSearch] = useState(getTrimmedSearchQuery());
 
-  private readonly handleSearch = (search: string): void => {
-    this.setState({ search });
+  useEffect(() => {
+    preloadMedia();
+  }, []);
+
+  const handleSearch = (search: string): void => {
+    setSearch(search);
   };
 
-  private preloadMedia(): void {
-    for (const source of [portal, errorImage]) {
-      const img = new Image();
-      img.src = source;
-    }
-  }
-}
+  return (
+    <div className="w-full p-10">
+      <Header onSearch={handleSearch} />
+      <CardList search={search} />
+    </div>
+  );
+};
 
-export default App;
+export default React.memo(App);
