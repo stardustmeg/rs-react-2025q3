5bed3b57c7db8fe5c3fd25697c655ceec063c8af
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-07-07 06:28:42 +0300

    feat: update meta tags

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,8 +1,14 @@
 import React, { type JSX, useState } from 'react';
 
+import portal from '@/assets/gif/portal-rick-and-morty.gif';
+import errorImage from '@/assets/png/rick_and_morty.png';
 import CardList from '@/components/CardList';
 import Header from '@/components/Header';
 import { getTrimmedSearchQuery } from '@/services/localStorage';
+
+for (const source of [portal, errorImage]) {
+  new Image().src = source;
+}
 
 const App = (): JSX.Element => {
   const [search, setSearch] = useState(getTrimmedSearchQuery());
