96deec81a96426a3593c4f17d1772567f7e82007
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-07-24 20:59:35 +0300

    style: formatting

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,10 @@
 /** @type {import("prettier").Config} */
 const config = {
-  "printWidth": 120,
-  "singleQuote": true,
-  "trailingComma": "all",
-  "plugins": ["prettier-plugin-tailwindcss"],
-  "tailwindStylesheet": "./src/styles/main.css"
-}
+  printWidth: 120,
+  singleQuote: true,
+  trailingComma: 'all',
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindStylesheet: './src/styles/main.css',
+};
 
 export default config;
