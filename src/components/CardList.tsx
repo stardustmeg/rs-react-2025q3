3462af9405987443dd4d65063b58144404d26284
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-07-27 20:48:21 +0300

    fix: no results layout

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -9,17 +9,16 @@ interface CardListProps {
 
 const CardList: React.FC<CardListProps> = ({ characters }: CardListProps) => {
   return (
-    <div className="min-h-screen">
-      <div
-        className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
-        data-testid="card-list"
-      >
-        {characters.length ? (
-          characters.map((char) => <CharacterCard character={char} key={char.id} />)
-        ) : (
-          <NoResultsFound />
-        )}
-      </div>
+    <div className="min-h-screen" data-testid="card-list">
+      {characters.length > 0 ? (
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {characters.map((char) => (
+            <CharacterCard character={char} key={char.id} />
+          ))}
+        </div>
+      ) : (
+        <NoResultsFound />
+      )}
     </div>
   );
 };
