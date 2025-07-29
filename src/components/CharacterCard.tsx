ac01a00477bc54bfb97efc6bbbc03c84bab3f669
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-07-30 01:39:02 +0300

    feat: select and store character cards

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -4,23 +4,47 @@ import { Link, useSearchParams } from 'react-router';
 import type { TransformedCharacter } from '@/types';
 
 import CharacterImage from '@/components/CharacterImage';
+import useStore from '@/store';
 
 interface CharacterCardProps {
   character: TransformedCharacter;
 }
 
+const handleCheckboxClick = (event: React.MouseEvent): void => {
+  event.stopPropagation();
+};
+
 const CharacterCard: React.FC<CharacterCardProps> = ({ character }) => {
   const { id, image, info, name } = character;
   const [searchParams] = useSearchParams();
 
+  const { isCharacterSelected, toggleSelectedCharacter } = useStore();
+  const isSelected = isCharacterSelected(character);
+
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    event.stopPropagation();
+    toggleSelectedCharacter(character);
+  };
+
   return (
-    <Link to={`${id}?${searchParams}`}>
-      <article
-        className="mx-auto h-full w-full rounded-lg bg-white p-2 shadow-md transition-transform duration-300 hover:scale-101"
-        data-testid="character-card"
-      >
+    <article
+      className="mx-auto h-full w-full rounded-lg bg-white p-2 shadow-md transition-transform duration-300 hover:scale-101"
+      data-testid="character-card"
+    >
+      <Link to={`${id}?${searchParams}`}>
         <div className="flex flex-col items-center rounded-t-lg bg-custom-beige p-4">
-          <p className="mb-2 text-center text-lg font-semibold">{name}</p>
+          <div className="flex place-content-center gap-2">
+            <div className="flex place-content-center">
+              <input
+                checked={isSelected}
+                data-testid="character-checkbox"
+                onChange={handleCheckboxChange}
+                onClick={handleCheckboxClick}
+                type="checkbox"
+              />
+            </div>
+            <p className="mb-2 text-center text-lg font-semibold">{name}</p>
+          </div>
           <CharacterImage alt={name} src={image} />
         </div>
         <div className="space-y-2 rounded-b-lg bg-white py-4 text-sm">
@@ -30,8 +54,8 @@ const CharacterCard: React.FC<CharacterCardProps> = ({ character }) => {
             </div>
           ))}
         </div>
-      </article>
-    </Link>
+      </Link>
+    </article>
   );
 };
 
