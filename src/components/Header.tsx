17b75e27b0a6b52b9e76c348536deda25f649285
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-07-31 16:15:08 +0300

    feat: implement theme switch

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import type React from 'react';
 
 import Navigation from '@/components/Navigation';
 import Search from '@/components/Search';
+import ThemeSwitch from '@/components/ThemeSwitch';
 
 interface HeaderProps {
   handleSearch: (query: string) => void;
@@ -9,11 +10,17 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ handleSearch, initialSearchQuery }) => (
-  <header className="fixed top-0 left-0 z-50 w-full bg-custom-pink px-6 py-4 shadow-md" data-testid="header">
+  <header
+    className="fixed top-0 left-0 z-50 w-full bg-custom-pink px-6 py-4 shadow-md dark:bg-dark-header"
+    data-testid="header"
+  >
     <div className="mx-auto flex flex-wrap items-center justify-center gap-4 md:justify-between">
       <Search handleSearch={handleSearch} initialSearchQuery={initialSearchQuery} />
 
-      <Navigation />
+      <div className="flex items-center gap-4">
+        <Navigation />
+        <ThemeSwitch />
+      </div>
     </div>
   </header>
 );
