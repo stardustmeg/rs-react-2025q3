eff51b4183e56f3020df8f2186dfb5ba6a4d1739
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-07-31 16:26:06 +0300

    feat: implement generic local storage hook

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,15 +1,38 @@
+/* eslint-disable @typescript-eslint/consistent-type-assertions */
 import { useEffect, useState } from 'react';
 
 const LS_SEARCH_KEY = 'search';
 const LS_PREFIX = 'stardustmeg_8c0a1a24-b273-4b98-91c6-c7d623fc53f1';
 
 const getFullKey = (key: string): string => {
-  return `${LS_PREFIX}_${key}`;
+  return `${key}_${LS_PREFIX}`;
+};
+
+export const useLocalStorage = <T>(key: string, defaultValue: T): [T, (value: T) => void] => {
+  const [value, setValue] = useState<T>(() => {
+    const storedValue = localStorage.getItem(getFullKey(key));
+    if (storedValue === null) {
+      return defaultValue;
+    }
+
+    try {
+      return JSON.parse(storedValue) as T;
+    } catch {
+      return storedValue as unknown as T;
+    }
+  });
+
+  useEffect(() => {
+    const valueToStore = typeof value === 'string' ? value : JSON.stringify(value);
+    localStorage.setItem(getFullKey(key), valueToStore);
+  }, [key, value]);
+
+  return [value, setValue];
 };
 
 type UseLocalStorageReturnType = [string, (value: string) => void];
 
-export const useLocalStorage = (): UseLocalStorageReturnType => {
+export const useLocalStorageSearch = (): UseLocalStorageReturnType => {
   const [searchQuery, setSearchQuery] = useState<string>(() => {
     const storedValue = localStorage.getItem(getFullKey(LS_SEARCH_KEY));
     return storedValue?.trim() ?? '';
