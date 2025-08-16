3c6a72d55740f3f01c618dd6f8c9a0dc319b82d5
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-08-16 15:44:55 +0300

    build: remove tests

--- src/__mocks__/mockInfo.ts
+++
@@ -1,8 +0,0 @@
-import type { Character, Info } from '@/types';
-
-import { mockCharacters } from '@/__mocks__/mockCharacters';
-
-export const mockInfo: Info<Character[]> = {
-  info: { count: 0, next: null, pages: 0, prev: null },
-  results: mockCharacters,
-};
