08c1d045a587f9407919c472281a7fe3ff6dbd59
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-08-17 21:45:23 +0300

    feat: add japanese

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -30,7 +30,7 @@ const Search: React.FC<SearchProps> = ({ handleSearch, initialSearchQuery }) => 
   };
 
   return (
-    <form className="flex w-full place-items-center gap-2 md:max-w-2/3 lg:max-w-1/3" onSubmit={handleSubmit}>
+    <form className="flex w-full place-items-center gap-2 lg:max-w-1/2" onSubmit={handleSubmit}>
       <div className="relative w-full">
         <input
           className="w-full rounded border border-custom-green bg-custom-blue p-2 pr-7 text-sm text-custom-dark-night shadow-sm focus:border-custom-green focus:ring-2 focus:ring-custom-green focus:outline-none dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:focus:border-custom-green"
@@ -43,7 +43,7 @@ const Search: React.FC<SearchProps> = ({ handleSearch, initialSearchQuery }) => 
         <ClearButton onClick={handleClear} visible={!!query} />
       </div>
       <button
-        className="button rounded bg-custom-yellow px-4 py-2 text-custom-coal shadow-sm dark:bg-custom-green dark:text-white"
+        className="w-1/3 button rounded bg-custom-yellow px-4 py-2 text-custom-coal shadow-sm dark:bg-custom-green dark:text-white"
         type="submit"
       >
         {t('button')}
