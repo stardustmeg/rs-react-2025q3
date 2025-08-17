af8b11e6e202bcc7ec3728f3dd1aff5c6273c637
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-08-17 22:51:58 +0300

    feat: update not found

--- app/global-not-found.tsx
+++ app/global-not-found.tsx
@@ -1,24 +1,32 @@
-import type { JSX } from 'react';
-
 import { useTranslations } from 'next-intl';
+import Image from 'next/image';
 import Link from 'next/link';
+import { type JSX } from 'react';
 
-export default function GlobalNotFound(): JSX.Element {
+import '@/app/globals.css';
+import errorImage from '@/assets/png/rick_and_morty_eyes.png';
+
+export default function NotFound(): JSX.Element {
   const t = useTranslations('notFound');
 
   return (
     <html lang="en">
       <body>
-        <div className="flex min-h-screen flex-col items-center justify-center space-y-6 p-6 text-center">
-          <div className="mx-auto flex max-w-[80%] flex-col place-items-center gap-4 transition-all duration-300">
-            <h1 className="text-3xl font-bold text-custom-coal dark:text-custom-gray">{t('title')}</h1>
+        <div className="flex min-h-[calc(100vh-80px)] flex-col items-center justify-center space-y-6 p-6 text-center">
+          <div className="flex max-w-[80%] flex-col place-items-center gap-4 transition-all duration-300">
+            <Image
+              alt="Error illustration"
+              className="h-auto max-w-2/3"
+              height={400}
+              priority
+              src={errorImage}
+              width={400}
+            />
+            <h1 className="text-3xl font-bold text-custom-coal">{t('title')}</h1>
 
-            <p className="max-w-md text-lg text-custom-coal dark:text-custom-gray">{t('description')}</p>
+            <p className="max-w-md text-lg text-custom-coal">{t('description')}</p>
 
-            <Link
-              className="text-xl font-bold text-custom-coal transition-all duration-300 hover:underline dark:text-custom-green dark:hover:text-custom-yellow"
-              href="/"
-            >
+            <Link className="text-xl font-bold text-custom-coal transition-all duration-300 hover:underline" href="/">
               {t('backHome')}
             </Link>
           </div>
