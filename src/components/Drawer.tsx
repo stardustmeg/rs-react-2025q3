027ab96298c13feca0f85315c09c99d988d4c90f
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-08-17 15:41:45 +0300

    feat: character not found page

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -29,18 +29,14 @@ const Drawer: React.FC<DrawerProps> = ({ children }) => {
   const handleClose = (): void => {
     dialogReference.current?.close();
 
-    if (document.referrer.includes(window.location.origin)) {
-      router.back();
-    } else {
-      const pathSegments = pathname.split('/');
-      const basePathSegments = pathSegments.slice(0, -1);
-      const basePath = basePathSegments.join('/');
+    const pathSegments = pathname.split('/');
+    const basePathSegments = pathSegments.slice(0, -1);
+    const basePath = basePathSegments.join('/');
 
-      const queryString = searchParams.toString();
-      const newUrl = queryString ? `${basePath}?${queryString}` : basePath;
+    const queryString = searchParams.toString();
+    const newUrl = queryString ? `${basePath}?${queryString}` : basePath;
 
-      router.replace(newUrl);
-    }
+    router.replace(newUrl, { scroll: false });
   };
 
   return (
@@ -51,12 +47,12 @@ const Drawer: React.FC<DrawerProps> = ({ children }) => {
       ref={dialogReference}
     >
       <div
-        className="fixed right-0 top-0 h-full w-3/4 bg-white shadow-lg transition-transform duration-300 dark:bg-dark-card sm:w-1/2 md:w-1/3"
+        className="fixed top-0 right-0 h-full w-3/4 bg-white shadow-lg transition-transform duration-300 sm:w-1/2 md:w-1/3 dark:bg-dark-card"
         onClick={stopPropagation}
       >
         <button
           aria-label="Close drawer"
-          className="absolute right-4 top-4 z-50 text-2xl text-gray-500 hover:cursor-pointer hover:text-gray-700 focus:outline-none dark:text-gray-400 dark:hover:text-gray-200"
+          className="absolute top-4 right-4 z-50 text-2xl text-gray-500 hover:cursor-pointer hover:text-gray-700 focus:outline-none dark:text-gray-400 dark:hover:text-gray-200"
           onClick={handleClose}
         >
           &times;
