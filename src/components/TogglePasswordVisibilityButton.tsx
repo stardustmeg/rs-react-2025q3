91772f381720fa6050e907bf5843dba2d79955c8
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-08-24 20:07:01 +0300

    test: toggle password visibility button

--- src/components/TogglePasswordVisibilityButton.tsx
+++ src/components/TogglePasswordVisibilityButton.tsx
@@ -15,7 +15,9 @@ export function TogglePasswordVisibilityButton({
     <button
       aria-label={showPassword ? 'Hide password' : 'Show password'}
       className={styles.passwordToggle}
-      onClick={togglePasswordVisibility}
+      onClick={() => {
+        togglePasswordVisibility();
+      }}
       type="button"
     >
       {showPassword ? '🙈' : '👁️'}
