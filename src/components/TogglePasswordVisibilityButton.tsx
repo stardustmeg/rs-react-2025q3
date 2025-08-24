66bb2717778e667fed8142fc2544e4c6f2f16f80
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-08-24 04:18:55 +0300

    feat: toggle password visibility

---
+++ src/components/TogglePasswordVisibilityButton.tsx
@@ -0,0 +1,24 @@
+import type { JSX } from 'react';
+
+import styles from '@/styles/Form.module.css';
+
+interface TogglePasswordVisibilityButtonProps {
+  showPassword: boolean;
+  togglePasswordVisibility: () => void;
+}
+
+export function TogglePasswordVisibilityButton({
+  showPassword,
+  togglePasswordVisibility,
+}: TogglePasswordVisibilityButtonProps): JSX.Element {
+  return (
+    <button
+      aria-label={showPassword ? 'Hide password' : 'Show password'}
+      className={styles.passwordToggle}
+      onClick={togglePasswordVisibility}
+      type="button"
+    >
+      {showPassword ? '🙈' : '👁️'}
+    </button>
+  );
+}
